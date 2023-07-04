07fa9825c9eaa91900de9bbe74c74ec9d4e61a63
audwo <kmjstj3@naver.com>
2023-07-04 21:29:05 +0900

    Create Listpage

---
+++ src/pages/ListPage.js
@@ -0,0 +1,14 @@
+import DiaryList from "../components/DiaryList";
+
+import "./ListPage.css";
+
+const ListPage = () => {
+  return (
+    <div class="ListPage">
+      <h3>List page</h3>
+      <DiaryList />
+    </div>
+  );
+};
+
+export default ListPage;
