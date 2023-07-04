17b88b0998bb6900a36dc84bcd306548bda3afcc
audwo <kmjstj3@naver.com>
2023-07-04 21:31:08 +0900

    separate OptionMenu

---
+++ src/components/OptionMenu.js
@@ -0,0 +1,13 @@
+const OptionMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default OptionMenu;
