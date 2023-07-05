bd793fb3b554c419e8f067011d50e21fce1b43c8
audwo <kmjstj3@naver.com>
2023-07-05 20:14:25 +0900

    Create New.js

---
+++ src/pages/New.js
@@ -0,0 +1,12 @@
+import AddItem from "../components/AddItem";
+
+const New = () => {
+  return (
+    <div class="New">
+      <h3>Add your story</h3>
+      <AddItem />
+    </div>
+  );
+};
+
+export default New;
